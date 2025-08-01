6460dbf72a2d4506d2718682c8504f4733ef5e3e
Adolph <1138027478@qq.com>
2025-08-01 14:23:26 +0800

    add 增加抓取方式 只抓取固定的域名信息

--- src/main/component/proxy/rule.js
+++ src/main/component/proxy/rule.js
@@ -1,41 +1,63 @@
 // anyproxy的规则文件.
+function isCrawlHost(host) {
+  const domains = ['meituan.com', 'ele.me', 'meituan.net']
+
+  for(let k in domains) {
+    if(host.indexOf(domains[k]) > -1) {
+      return true
+    }
+  }
+
+  return false
+}
+
 module.exports = {
   // 拦截请求
   beforeSendRequest(requestDetail) {
-    console.log(requestDetail.url)
-    // 示例：修改所有百度请求的 User-Agent
-    if (requestDetail.url.includes('baidu.com')) {
-      const newRequestOptions = requestDetail.requestOptions
-      newRequestOptions.headers['User-Agent'] = 'Electron-AnyProxy-Client'
+    const link_info = new URL(requestDetail.url)
+    if(!isCrawlHost(link_info.host)) {
       return {
-        requestOptions: newRequestOptions
+        requestOptions: requestDetail.requestOptions
       }
     }
+
+    // 这里可以增加一些其他的东西过来. 例如使用的哪个代理撒的. 先不管吧. 我们只是拦截其他的网络请求而已.
     return {
       requestOptions: requestDetail.requestOptions
     }
   },
 
-  // 拦截响应
+  // 拦截响应. 仅仅只抓取固定的域名信息.
   beforeSendResponse(requestDetail, responseDetail) {
-    // 示例：修改 JSON 响应
-    console.log("=================== response detail =====================")
-    console.log(responseDetail.response.body.toString())
-    // 这里只负责解析指定的数据信息. 其他不管. 例如只解析京东的数据.
-    if (requestDetail.url.includes('api.example.com')) {
-      const newResponse = responseDetail.response
-      try {
-        const body = JSON.parse(newResponse.body.toString())
-        // 在响应中添加自定义字段
-        body.proxyBy = 'Electron-AnyProxy'
-        newResponse.body = Buffer.from(JSON.stringify(body))
-        return {
-          response: newResponse
-        }
-      } catch (e) {
-        // 非 JSON 响应不处理
+    // 这里可以只获取指定的json地址就可以了.
+    const link_info = new URL(requestDetail.url)
+    if(!isCrawlHost(link_info.host)) {
+      return {
+        response: responseDetail.response
       }
     }
+    
+    // 这里要进行一次记录了. 只是记录就可以了. 记录类型是json的. 先不管图片和数据.
+    const headers = responseDetail.response.header
+
+    if(Object.keys(headers).indexOf('Content-Type') == -1) {
+      return {
+        response: responseDetail.response
+      }
+    }
+
+    const content_type = headers['Content-Type']
+    // 只要json数据. 其他不管了. 后面用来做分析使用的.
+    if(content_type.indexOf('application/json') <= -1) {
+      return {
+        response: responseDetail.response
+      }
+    }
+
+    // 这里开始保存. 可以放到另外一个地方了. 后面来看. @todo
+    console.log('=============================================================')
+    console.log(requestDetail.url)
+    console.log(responseDetail.response.body.toString())
     return {
       response: responseDetail.response
     }
