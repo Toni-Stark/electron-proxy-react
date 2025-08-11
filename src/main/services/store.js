0c7ceaa531fc08dc665c8126f0172fb0134c852d
Adolph <1138027478@qq.com>
2025-08-11 13:32:48 +0800

    add 完成店铺删除相关东西

--- src/main/services/store.js
+++ src/main/services/store.js
@@ -1,7 +1,12 @@
 const Shop = require('../models/shop')
+const ShopSpuTag = require('../models/shop_spu_tag')
+const ShopSpu = require('../models/shop_spu')
+const ShopSku = require('../models/shop_sku')
+const ShopTag = require('../models/shop_tag')
+
 const { Op } = require('sequelize')
 const { PAGE_SIZE } = require('./constants')
-const { renderSuc,  renderFail} = require('../component/web/response')
+const { renderSuc,  renderFail } = require('../component/web/response')
 
 export async function getStoreList(kw = '', platform = '', page = 1) {
   let cond = {}
@@ -61,4 +66,43 @@ export async function getStoreInfo(id) {
   }
 
   return renderSuc(store_info)
+}
+
+export async function delStore(id) {
+  if(!id) {
+    return renderFail('请输入关键信息')
+  }
+
+  // 开始删除表信息. 
+  const store_info = await Shop.findOne({
+    where: {
+      id: id
+    },
+    raw: true,
+  })
+
+  if(!store_info) {
+    return renderFail('未找到店铺信息')
+  }
+
+  // 开始删除表信息.
+  // 1. 店铺信息
+  // 2. spu信息.
+  // 3. sku信息
+  // 4. tag信息
+  // 5. tag关联信息
+  const cond = {
+    where: {
+      platform: store_info.platform,
+      third_id: store_info.third_id
+    }
+  }
+
+  await ShopSpuTag.destroy(cond)
+  await ShopSku.destroy(cond)
+  await ShopSpu.destroy(cond)
+  await ShopTag.destroy(cond)
+  await Shop.destroy(cond)
+
+  return renderSuc('删除完成')
 }
