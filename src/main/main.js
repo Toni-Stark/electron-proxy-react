44d40bfc6e6cdac86e1df72b0b905fa6891be80c
Adolph <1138027478@qq.com>
2025-08-13 15:46:44 +0800

    add 增加美团分享的这种形式

--- src/main/main.js
+++ src/main/main.js
@@ -5,6 +5,8 @@ const jobs = require('./jobs')
 const listener = require('./listener')
 const { stopProxy } = require('./component/proxy/anyproxy')
 
+const gotTheLock = app.requestSingleInstanceLock();
+
 // 保持对窗口对象的全局引用，如果不这样做，当JavaScript对象被垃圾回收时，窗口将自动关闭
 let mainWindow
 
@@ -52,18 +54,23 @@ function createWindow() {
   listener()
 }
 
-// Electron 会在初始化后并准备好创建浏览器窗口时调用这个函数
-// 部分 API 在 ready 事件触发后才能使用。
-app.on('ready', createWindow)
 
-// 当所有窗口都关闭时退出
-app.on('window-all-closed', function () {
-  // 在 macOS 上，除非用户用 Cmd + Q 确定退出，否则绝大部分应用及其菜单栏会保持激活
-  if (process.platform !== 'darwin') app.quit()
-})
+if(!gotTheLock) {
+  app.quit()
+} else {
+  // Electron 会在初始化后并准备好创建浏览器窗口时调用这个函数
+  // 部分 API 在 ready 事件触发后才能使用。
+  app.on('ready', createWindow)
 
-app.on('activate', function () {
-  // 在 macOS 上，当点击 dock 图标并且没有其他窗口打开时，通常会重新创建一个窗口
-  if (mainWindow === null) createWindow()
-})
+  // 当所有窗口都关闭时退出
+  app.on('window-all-closed', function () {
+    // 在 macOS 上，除非用户用 Cmd + Q 确定退出，否则绝大部分应用及其菜单栏会保持激活
+    if (process.platform !== 'darwin') app.quit()
+  })
+
+  app.on('activate', function () {
+    // 在 macOS 上，当点击 dock 图标并且没有其他窗口打开时，通常会重新创建一个窗口
+    if (mainWindow === null) createWindow()
+  })
+}
 
